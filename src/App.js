af0072946578687a3258a99002ac1ec684a4736f
Luciano Amâncio <madruga665@hotmail.com>
2021-09-21 12:42:03 -0300

    feat: component ContactList

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import ContactList from "./components/ContactList";
 
 function App() {
-  const [contacts, setContacts] = useState([]);
-  const fetchContacts = async () => {
-    const response = await fetch("http://localhost:3001/contacts");
-    const data = await response.json();
-    setContacts(data);
-  };
-
-  useEffect(() => {
-    fetchContacts();
-  }, []);
-
   return (
-    <div>
-      <h1>lista de contatos</h1>
-      <ul>
-        {contacts.map(({first_name, last_name}, index) => (
-          <li key={first_name + index}>{`${first_name} ${last_name}`}</li>
-        ))}
-      </ul>
-    </div>
+    <ContactList/>
   );
 }
 
---
+++ src/components/ContactList/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+
+export default function ContactList() {
+  const [contacts, setContacts] = useState([]);
+  const fetchContacts = async () => {
+    const response = await fetch("http://localhost:3001/contacts");
+    const data = await response.json();
+    setContacts(data);
+  };
+
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
+  return (
+    <div>
+      <h1>lista de contatos</h1>
+      <ul>
+        {contacts.map(({first_name, last_name}, index) => (
+          <li key={first_name + index}>{`${first_name} ${last_name}`}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
